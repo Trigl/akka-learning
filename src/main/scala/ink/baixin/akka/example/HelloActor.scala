9451776b49951bc3b63a13b7ea32f0c83a3bdf16
Trigl <18810690994@163.com>
2018-05-21 09:53:16 +0800

    Hello actor.

---
+++ src/main/scala/ink/baixin/akka/example/HelloActor.scala
@@ -0,0 +1,26 @@
+package ink.baixin.akka.example
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+class HelloActor extends Actor {
+  def receive = {
+    case "hello" => println("hello back at you")
+    case _ => println("huh?")
+  }
+}
+
+object Main extends App {
+
+  // an actor needs an ActorSystem
+  val system = ActorSystem("HelloSystem")
+
+  // create and start an antor
+  val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
+
+  // send the actor two messages
+  helloActor ! "hello"
+  helloActor ! "buenos dias"
+
+  // shut down the system
+  system.terminate
+}
